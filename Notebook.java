b879c5e741a4b93a939c71eb7deec28ca41b80c3
cdwheeless <cwheeless@bellsouth.net>
2016-04-06 15:54:32 -0500

    Added files via upload

--- Notebook.java
+++ Notebook.java
@@ -2,6 +2,7 @@
 //3-18
 package eng;
 
+import java.io.File;
 import java.util.*;
 
 /*
@@ -13,9 +14,55 @@ public class Notebook
 {
 	private ArrayList<Note> notes;
 	
+	public Notebook()
+	{
+		notes = new ArrayList<Note>();
+	}
+	
 	public Notebook(Note n)
 	{
 		this.notes.add(n);
+	}
+	
+	public void save()
+	{
+		for(Note temp : notes)
+		{
+			temp.save();
+		}
+	}
+	
+	public void load()
+	{
+		File dir = new File("Notes/");
+		File[] allFiles = dir.listFiles();
+		
+		Note n = new Note();
+		String title;
+		
+		for(File f : allFiles)
+		{
+			title = f.getName();
+			n.load(noExtension(title));
+			notes.add(n);
+		}
+	}
+	
+	private String noExtension(String s)
+	{
+		String result = "";
+		char index;
+		
+		for(int i = 0; i < s.length(); i++)
+		{
+			index = s.charAt(i);
+			if(index == '.')
+			{
+				result = s.substring(0, i);
+				break;
+			}
+		}
+		return result;
 	}
 	
 	public void addNote(Note n)
